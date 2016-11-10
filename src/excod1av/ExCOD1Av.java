ce509dc2d2673b5a8599c5d008f62900ce5821c0
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-11-10 08:47:33 +0100

    Paso A clonamos el proyecto y realizamos un primer commit a nuestro repositorio

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -15,7 +15,7 @@ public class ExCOD1Av {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Clonamos el repositorio
     }
     
 }
